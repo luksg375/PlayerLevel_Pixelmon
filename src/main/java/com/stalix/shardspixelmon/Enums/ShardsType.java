4c3f356f3b0b65fabde8b70107c98e128b80fa23
Lucas Gabriel <lucasgabrielsantos398@gmail.com>
2024-04-30 18:14:07 -0300

    commit itens shards

---
+++ src/main/java/com/stalix/shardspixelmon/Enums/ShardsType.java
@@ -0,0 +1,22 @@
+package com.stalix.shardspixelmon.Enums;
+
+public enum ShardsType {
+    NORMAL,
+    FIRE,
+    WATER,
+    ELECTRIC,
+    GRASS,
+    ICE,
+    FIGHTING,
+    POISON,
+    GROUND,
+    FLYING,
+    PSYCHIC,
+    BUG,
+    ROCK,
+    GHOST,
+    DRAGON,
+    DARK,
+    STEEL,
+    FAIRY
+}
--- src/main/java/com/stalix/shardspixelmon/items/Items.java
+++ src/main/java/com/stalix/shardspixelmon/items/Items.java
@@ -1,7 +1,10 @@
 package com.stalix.shardspixelmon.items;
 
+import com.stalix.shardspixelmon.Enums.ShardsType;
 import com.stalix.shardspixelmon.ModFile;
+import com.stalix.shardspixelmon.items.custom.Shard;
 import net.minecraft.item.Item;
+import net.minecraft.item.Rarity;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,8 +15,24 @@ public class Items {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, ModFile.MOD_ID);
 
-    public static final RegistryObject<Item> MONEY = ITEMS.register("money", () -> new Item(new Item.Properties()));
-
+    public static final RegistryObject<Item> FIRE_SHARD = ITEMS.register("fire_shard", () -> new Shard(new Item.Properties().rarity(Rarity.RARE), ShardsType.FIRE));
+    public static final RegistryObject<Item> WATER_SHARD = ITEMS.register("water_shard", () -> new Shard(new Item.Properties().rarity(Rarity.RARE), ShardsType.FIRE));
+    public static final RegistryObject<Item> GRASS_SHARD = ITEMS.register("grass_shard", () -> new Shard(new Item.Properties().rarity(Rarity.RARE), ShardsType.FIRE));
+    public static final RegistryObject<Item> STEEL_SHARD = ITEMS.register("steel_shard", () -> new Shard(new Item.Properties().rarity(Rarity.RARE), ShardsType.FIRE));
+    public static final RegistryObject<Item> ROCK_SHARD = ITEMS.register("rock_shard", () -> new Shard(new Item.Properties().rarity(Rarity.RARE), ShardsType.FIRE));
+    public static final RegistryObject<Item> NORMAL_SHARD = ITEMS.register("normal_shard", () -> new Shard(new Item.Properties().rarity(Rarity.RARE), ShardsType.FIRE));
+    public static final RegistryObject<Item> ELECTRIC_SHARD = ITEMS.register("electric_shard", () -> new Shard(new Item.Properties().rarity(Rarity.RARE), ShardsType.FIRE));
+    public static final RegistryObject<Item> ICE_SHARD = ITEMS.register("ice_shard", () -> new Shard(new Item.Properties().rarity(Rarity.RARE), ShardsType.FIRE));
+    public static final RegistryObject<Item> FIGHTING_SHARD = ITEMS.register("fighting_shard", () -> new Shard(new Item.Properties().rarity(Rarity.RARE), ShardsType.FIRE));
+    public static final RegistryObject<Item> POISON_SHARD = ITEMS.register("poison_shard", () -> new Shard(new Item.Properties().rarity(Rarity.RARE), ShardsType.FIRE));
+    public static final RegistryObject<Item> GROUND_SHARD = ITEMS.register("ground_shard", () -> new Shard(new Item.Properties().rarity(Rarity.RARE), ShardsType.FIRE));
+    public static final RegistryObject<Item> FLYING_SHARD = ITEMS.register("flying_shard", () -> new Shard(new Item.Properties().rarity(Rarity.RARE), ShardsType.FIRE));
+    public static final RegistryObject<Item> PSYCHIC_SHARD = ITEMS.register("psychic_shard", () -> new Shard(new Item.Properties().rarity(Rarity.RARE), ShardsType.FIRE));
+    public static final RegistryObject<Item> BUG_SHARD = ITEMS.register("bug_shard", () -> new Shard(new Item.Properties().rarity(Rarity.RARE), ShardsType.FIRE));
+    public static final RegistryObject<Item> GHOST_SHARD = ITEMS.register("ghost_shard", () -> new Shard(new Item.Properties().rarity(Rarity.RARE), ShardsType.FIRE));
+    public static final RegistryObject<Item> DRAGON_SHARD = ITEMS.register("dragon_shard", () -> new Shard(new Item.Properties().rarity(Rarity.RARE), ShardsType.FIRE));
+    public static final RegistryObject<Item> DARK_SHARD = ITEMS.register("dark_shard", () -> new Shard(new Item.Properties().rarity(Rarity.RARE), ShardsType.FIRE));
+    public static final RegistryObject<Item> FAIRY_SHARD = ITEMS.register("fairy_shard", () -> new Shard(new Item.Properties().rarity(Rarity.RARE), ShardsType.FIRE));
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
